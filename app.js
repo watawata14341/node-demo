adac9087eb28809ea96558428c07bdffe8284d09
wata14341 <akki.ni.ni.32@gmail.com>
2020-11-08 22:53:22 +0900

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('hello.ejs');
+});
+
+app.get('/top', (req, res) => {
+    res.render('top.ejs');
+});
+
+app.listen(3000);
