31661695c7aaf77650dbeef68367f43ad0152c1e
wata14341 <akki.ni.ni.32@gmail.com>
2020-11-27 23:19:19 +0900

    update

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
-
 const mysql = require('mysql');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'progate',
-    password: 'password',
+    user: 'root',
+    password: '',
     database: 'list_app'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
 });
 
 app.get('/', (req, res) => {
@@ -17,14 +25,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/index', (req, res) => {
-    // データベースからデータを取得する処理を書いてください
     connection.query(
       'SELECT * FROM items',
       (error, results) =>{
         console.log(results)
-        res.render('index.ejs');
+        res.render('index.ejs',{items: results});
       }
     );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  console.log(req.body.itemName)
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) =>{
+      res.redirect('/index');
+    }
+  );
 });
 
 app.listen(3000);
